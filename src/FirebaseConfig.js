c6b4335789f04fe7d870c570b8dada286fd6313e
Raymond Habib <raymondhabib936@gmail.com>
2025-09-07 16:13:12 +0400

    fixed major .env and firebase config error

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,12 +1,12 @@
 import {initializeApp} from "firebase/app";
 
 const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  appId: process.env.REACT_APP_APP_ID,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env/REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  appId: import.meta.env.VITE_APP_APP_ID,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
 };
 
 
